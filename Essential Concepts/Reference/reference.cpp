6f88d1da2e347683464eb44c14d295a81bfe5847
Mert Eldemir <mert1greatest@gmail.com>
2024-08-07 17:12:58 +0300

    reference update

--- Essential Concepts/Reference/reference.cpp
+++ Essential Concepts/Reference/reference.cpp
@@ -4,30 +4,6 @@
 
 using namespace std;
 
-/*
-When a variable is declared as a reference, it becomes an alternative name for an existing variable. A variable can be declared as a reference by putting ‘&’ in the declaration.
-*/
-
-/*
-There are multiple applications for references in C++, a few of them are mentioned below:
-
-* Modify the passed parameters in a function
-* Avoiding a copy of large structures
-* In For Each Loop to modify all objects
-* For Each Loop to avoid the copy of objects
-*/
-
-/*
-Limitations of References:
-* Once a reference is created, it cannot be later made to reference another object; it cannot be reset. This is often done with pointers.
-* References cannot be NULL. Pointers are often made NULL to indicate that they are not pointing to any valid thing.
-* A reference must be initialized when declared. There is no such restriction with pointers.
-
-*/
-
-// reference is basically another name (alias) for the same variable
-// references can be initizalized once and can not be assignment to another variable later
-
 // Function having parameters as references
 void swap(int &first, int &second)
 {
