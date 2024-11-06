9a5e0595954a50a1b4189938cde57ebc198946d1
Mert Eldemir <mert1greatest@gmail.com>
2024-11-06 22:09:45 +0300

    taylor series & horners method

--- Algorithms/Recursion/algorithms/taylorSeries.cpp
+++ Algorithms/Recursion/algorithms/taylorSeries.cpp
@@ -32,9 +32,46 @@ double taylorSeries(int x, int n) // For e^x series for n terms
     return r + p / f;
 }
 
+// Tayloe Series using Horner's Rule
+// how to reduce multiplications?
+
+// e^x: e^x = 1 + (x^1 / 1!) + (x^2 / 2!) + (x^3 / 3!) + ... + (x^n / n!)
+
+// ->  1 + x/2 * [1 + x/2 * [1+ x/3 + x^2/(3*4)] ...]
+
+// -> 1 + x * [1 + x/2 * [1 + x/3 * [1 + x/4] * n]] => O(n)
+
+double taylorHorner(int x, int n)
+{
+    double s = 1;
+
+    for (; n > 0; --n)
+    {
+        s = 1 + x * s / n;
+    }
+
+    return s;
+}
+
+double taylorHernerRec(int x, int n)
+{
+    static double s = 1;
+
+    if (n == 0)
+    {
+        return s;
+    }
+
+    s = 1 + x * s / n;
+
+    return taylorHernerRec(x, n - 1);
+}
+
 int main()
 {
-    cout << "taylorSeries: " << taylorSeries(1, 10); // 2.71828
+    cout << "taylorSeries: " << taylorSeries(1, 10) << endl;      // 2.71828
+    cout << "taylorHorner: " << taylorHorner(1, 4) << endl;       // 2.70833
+    cout << "taylorHernerRec: " << taylorHernerRec(1, 4) << endl; // 2.70833
 
     return 0;
 }
