78b856558d2d0e342aa4d5b4216369d92470be5d
Mert Eldemir <mert1greatest@gmail.com>
2025-02-17 23:53:32 +0300

    last updates

---
+++ Algorithms/Strings/permutationString.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// Permutation (all possible strings using characters of that string)
+
+/*
+    string = "abc"
+
+    "abc", "acb", "bac", "bca", "cab", "cba"
+    possible permutations = 3! = 6
+*/
+
+void permutation(char str[])
+{
+    
+}
+
+int main()
+{
+    char str[] = "abc";
+
+    std::cout << "all permutations of " << str << " is: ";
+    permutation(str);
+
+    return 0;
+}
