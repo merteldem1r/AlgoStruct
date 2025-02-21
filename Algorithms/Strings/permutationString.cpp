82bf6497b83a11de3b3ca6cab4a076322ca7ff4e
Mert Eldemir <mert1greatest@gmail.com>
2025-02-21 23:36:25 +0300

    string permutations algorithms

--- Algorithms/Strings/permutationString.cpp
+++ Algorithms/Strings/permutationString.cpp
@@ -10,7 +10,7 @@
 */
 
 // C style solution
-void permutation(char str[], int k) // Time: O(n!) Space: O(n)
+void permutation(char str[], int k) // Time: O(n * n!) Space: O(n)
 {
     static int A[10] = {0};
     static char res[10];
@@ -35,19 +35,37 @@ void permutation(char str[], int k) // Time: O(n!) Space: O(n)
     }
 }
 
-// Recursive solution
-void recursivePermutation(char str[], int l, int h) {
-    int i;
+// Recursive solutions
 
-    if (l == h) {
+// Reverse Lexicographic order
+void recursivePermutation(char str[], int l, int h) // Time: O(n * n!) Space: O(n)
+{
+    int i;
+    if (l == h)
         std::cout << str << " ";
-    } else {
-        for (i = l; i <= h; ++i) {
+    else
+        for (i = l; i <= h; ++i)
+        {
             std::swap(str[l], str[h]);
             recursivePermutation(str, l + 1, h);
             std::swap(str[l], str[i]);
         }
-    }
+}
+
+// Lexicographic order
+void recursivePermutation2(std::string &str, int l, int h) // Time: O(n * n!) Space: O(n)
+{
+    int i;
+
+    if (l == h)
+        std::cout << str << " ";
+    else
+        for (int i = l; i <= h; ++i)
+        {
+            std::swap(str[l], str[i]);
+            recursivePermutation2(str, l + 1, h);
+            std::swap(str[l], str[i]);
+        }
 }
 
 int main()
@@ -60,6 +78,14 @@ int main()
     std::cout << std::endl;
     std::cout << "all recursive permutations of " << str << " is: "; // cab cba bca bac acb abc
     recursivePermutation(str, 0, 2);
+
+    std::cout << std::endl;
+
+    std::string str2 = "abc";
+    std::cout << "all recursive permutations of " << str2 << " is: "; // abc acb bac bca cab cba
+    recursivePermutation2(str2, 0, 2);
+    std::cout << std::endl;
+    std::cout << str2;
 
     return 0;
 }
