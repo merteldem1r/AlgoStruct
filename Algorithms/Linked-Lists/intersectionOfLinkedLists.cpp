ab064484e7655320a4d18147e3e4334a9586e1c4
Mert Eldemir <mert1greatest@gmail.com>
2025-02-28 23:02:54 +0300

    last update

--- Algorithms/Linked-Lists/intersectionOfLinkedLists.cpp
+++ Algorithms/Linked-Lists/intersectionOfLinkedLists.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <unordered_map>
+#include <unordered_set>
 
 struct Node
 {
@@ -15,17 +15,17 @@ struct Node
 // 1) Method using Additional Space (unordered_map)
 Node *getIntersectionNode(Node *headA, Node *headB) // Time: O(n) Space: O(n)
 {
-    std::unordered_map<Node *, int> listMap;
+    std::unordered_set<Node *> listSet;
 
     while (headA != nullptr)
     {
-        listMap[headA]++;
+        listSet.insert(headA);
         headA = headA->next;
     }
 
     while (headB != nullptr)
     {
-        if (listMap.count(headB) > 0)
+        if (listSet.find(headB) != listSet.end())
             return headB;
 
         headB = headB->next;
@@ -34,8 +34,62 @@ Node *getIntersectionNode(Node *headA, Node *headB) // Time: O(n) Space: O(n)
     return nullptr;
 }
 
-// 2) Method using One Iteration
-Node *getIntersectionNode2(Node *headA, Node *headB)
+// 2) Method using Lists Length Difference
+class Solution
+{
+public:
+    int listLength(Node *head)
+    {
+        int len;
+        auto temp = head;
+        for (len = 0; temp != nullptr; temp = temp->next, ++len)
+        {
+        }
+        return len;
+    }
+
+    Node *skipList(Node *head, int skipCount)
+    {
+        auto temp = head;
+        for (int i = 0; i < skipCount; temp = temp->next, ++i)
+        {
+        }
+        return temp;
+    }
+
+    Node *intersection(Node *head1, Node *head2)
+    {
+        while (head1 != nullptr && head2 != nullptr)
+        {
+            if (head1 == head2)
+                return head1;
+
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+
+        return nullptr;
+    }
+
+    Node *getIntersectionNode(Node *headA, Node *headB) // Time: O(n) Space: O(1)
+    {
+        int headALen = listLength(headA); 
+        int headBLen = listLength(headB); 
+
+        int diff = headALen - headBLen;
+
+        if (diff == 0)
+            return intersection(headA, headB);
+
+        auto ptr1 = diff > 0 ? skipList(headA, std::abs(diff)) : headA;
+        auto ptr2 = diff > 0 ? headB : skipList(headB, std::abs(diff));
+
+        return intersection(ptr1, ptr2);
+    }
+};
+
+// 3) Method using Two Pointers (most efficient)
+Node *getIntersectionNode2(Node *headA, Node *headB) // Time: O(n) Space: O(1)
 {
     auto tempA = headA;
     auto tempB = headB;
