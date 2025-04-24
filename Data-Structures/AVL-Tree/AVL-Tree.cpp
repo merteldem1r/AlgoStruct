94e48a7b6ebe16864909beddde8e9fdf1f931884
Mert Eldemir <mert1greatest@gmail.com>
2025-04-24 23:24:21 +0300

    last updates

--- Data-Structures/AVL-Tree/AVL-Tree.cpp
+++ Data-Structures/AVL-Tree/AVL-Tree.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
-// AVL Trees
+// AVL Tree Data Structure
 
-int main () {
+/*
+
+    AVL trees guarantee **O(log n)** time for insertion, deletion, and lookup, by ensuring that the tree remains balanced at all times.
+
+        Balance Factor: height(left subtree) - height(right subtree)
+
+        Valid balance factor values:
+            -1: Right subtree is taller
+            0: Both subtrees are equal
+            +1: Left subtree is taller
+
+        If the balance factor becomes less than -1 or greater than +1, the tree needs rebalancing.
+
+*/
+
+int main()
+{
     return 0;
 }
