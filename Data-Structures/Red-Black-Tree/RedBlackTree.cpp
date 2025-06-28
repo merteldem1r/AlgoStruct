95b09a48514c794988ae2f9542aa9f1d8d8f8a5a
Mert Eldemir <mert1greatest@gmail.com>
2025-06-28 23:59:41 +0300

    last updates

--- Data-Structures/Red-Black-Tree/RedBlackTree.cpp
+++ Data-Structures/Red-Black-Tree/RedBlackTree.cpp
@@ -12,9 +12,110 @@
     5. Number of Blacks on Path from Root to the leas are same
     6. No 2 consecutive Red; Parent and Children of Red is Black
     7. New Inserted node is Red
-    8. Height is logN <= h <= 2logN 
+    8. Height is logN <= h <= 2logN
         * minimum logN, maximum is double of logN
+
+    * Color (Red & Black) is only need to maintain the balancing rule of the tree,
+
+    There is 2 methods to handle balance on the Red Black Tree:
+
+        1) Recoloring Case
+
+        RULE: If UNCLE is RED and the PARENT is RED use RECOLOR!
+
+            G (Black)
+            / \
+         P(R) U(R)
+         /
+        n(R)
+
+        G: Grandparent
+        P: Parent
+        U: Uncle
+        n: New inserted node
+
+        Problem:
+        - New red node (n) has red parent (P)
+        - P and U are both red -> violates property (no two reds in a row)
+
+        Fix:
+        - Recolor P and U to Black
+        - Recolor G to Red
+        - Move up to G and repeat fix if needed
+
+        After recoloring:
+
+            G (Red)
+            / \
+         P(B) U(B)
+         /
+        n(R)
+
+        2) Rotation Case
+
+        RULE: If PARENT is RED and UNCLE is BLACK or NULL (NULL also mean Black) use ROTATION!
+
+        Example: Left-Left Case (needs Right Rotation)
+
+            G (Black)
+            /     \
+          P(R)   U (Black or NIL)
+          /
+        n(R)
+
+
+        Problem:
+        - n and P are both red -> violates property
+        - Uncle is Black -> must rotate
+
+        Fix:
+        - Perform Right Rotation around G
+        - Recolor new root to Black, child to Red
+
+        Before rotation:
+
+            G(B)
+            /
+        P(R)
+        /
+        n(R)
+
+        After Right Rotation:
+
+            P(B)
+            /  \
+         n(R)   G(R)
+
+        Result:
+        - Tree structure changes
+        - Red-Black properties restored
+
 */
+
+enum Color
+{
+    RED,
+    BLACK
+};
+
+struct RBNode
+{
+    int value;
+    Color color;
+    RBNode *left;
+    RBNode *right;
+
+    RBNode(int value) : value(value), color(RED), right(nullptr), left(nullptr) {};
+};
+
+class RedBlackTree
+{
+private:
+    RBNode *Root;
+
+public:
+    RedBlackTree(int value) : Root(new RBNode(value)) {};
+};
 
 int main()
 {
