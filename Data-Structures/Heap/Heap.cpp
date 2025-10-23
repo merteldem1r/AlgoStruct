cb565ee553e520d2f8abe43881be94d69596a77c
Mert Eldemir <mert1greatest@gmail.com>
2025-10-23 15:32:08 +0300

    Heap reimplementing

--- Data-Structures/Heap/Heap.cpp
+++ Data-Structures/Heap/Heap.cpp
@@ -3,226 +3,81 @@
 // HEAP
 // AUTHOR: Mert Eldemir
 
+// Heap is a Complete Binary tree; so it's height is always logN
+// but Heap it's not useful for searching purpose
+
 class MaxHeap
 {
-    int *heapArr;
+private:
+    int *heapArr = nullptr;
     int capacity;
     int heapSize;
 
 public:
-    MaxHeap(int capacity);
+    MaxHeap(int cap)
+    {
+        this->capacity = cap;
+        this->heapSize = 0;
+        this->heapArr = new int[capacity];
+    }
 
-    MaxHeap(int capacity, int A[], int arrSize);
+    void Insert(int val) // Time: logN (as height of Heap) Space: O(1)
+    {
+        if (heapSize == capacity)
+        {
+            std::cout << "Heap is already full." << std::endl;
+            return;
+        }
 
-    void MaxHeapify(int i, int arrSize);
+        // insert at the end
+        int currIdx = heapSize;
+        heapArr[currIdx] = val;
 
-    void MaxHeapifyIterative(int i, int arrSize);
+        // move to top while inserted value is bigget than it's parents
+        while (currIdx >= 0 && heapArr[currIdx] > heapArr[parent(currIdx)])
+        {
+            std::swap(heapArr[currIdx], heapArr[parent(currIdx)]);
+            currIdx = parent(currIdx);
+        }
 
-    int extractMax();
+        ++heapSize;
+    }
 
-    int Parent(int i) { return (i - 1) / 2; };
+    // helpers
+    int left(int i) { return (2 * i) + 1; }
 
-    int Left(int i) { return (i * 2) + 1; };
+    int right(int i) { return (2 * i) + 2; }
 
-    int Right(int i) { return (i * 2) + 2; };
-
-    void insert(int key);
-
-    void print();
+    int parent(int i) { return (i - 1) / 2; }
 
     int getMax() { return heapArr[0]; }
 
-    int *getHeapArr() { return heapArr; }
-
-    // ~MaxHeap() { std::cout << "Deleting Heap\n"; };
-};
-
-MaxHeap::MaxHeap(int cap)
-{
-    heapSize = 0;
-    capacity = cap;
-    heapArr = new int[cap];
-}
-
-MaxHeap::MaxHeap(int cap, int A[], int arrSize)
-{
-    heapSize = arrSize;
-    capacity = cap;
-    heapArr = A;
-}
-
-void MaxHeap::insert(int key) // Time: O(log N)
-{
-    if (heapSize == capacity)
+    void printHeap()
     {
-        std::cout << "Could not insert, heap is already full." << std::endl;
-        return;
-    }
-
-    // First we insert the key at the end of the array
-    ++heapSize;
-    int i = heapSize - 1;
-    heapArr[i] = key;
-
-    // If the max heap property if violated then we fix it
-    while (i > 0 && heapArr[Parent(i)] < heapArr[i])
-    {
-        std::swap(heapArr[i], heapArr[Parent(i)]);
-        i = Parent(i);
-    }
-}
-
-void MaxHeap::MaxHeapify(int i, int arrSize) // Time: O(log N) Space: O(n)
-{
-    int l = Left(i);
-    int r = Right(i);
-
-    int largest = i;
-
-    if (l < arrSize && heapArr[l] > heapArr[largest])
-        largest = l;
-
-    if (r < arrSize && heapArr[r] > heapArr[largest])
-        largest = r;
-
-    if (largest != i)
-    {
-        std::swap(heapArr[i], heapArr[largest]);
-        MaxHeapify(largest, arrSize);
-    }
-}
-
-void MaxHeap::MaxHeapifyIterative(int i, int arrSize) // Time: O(log N) Space: O(1)
-{
-    while (true) {
-        int l = Left(i);
-        int r = Right(i);
-
-        int largest = i;
-
-        if (l < arrSize && heapArr[l] > heapArr[largest])
-            largest = l;
-
-        if (r < arrSize && heapArr[r] > heapArr[largest])
-            largest = r;
-
-        if (largest == i) {
-            break;
+        std::cout << "[ ";
+        for (int i = 0; i < heapSize; ++i)
+        {
+            std::cout << heapArr[i] << " ";
         }
-        
-        std::swap(heapArr[i], heapArr[largest]);
-        i = largest;
+        std::cout << "]";
     }
-}
-
-int MaxHeap::extractMax() // Time: O(log N)
-{
-    if (heapSize == 0)
-        return INT_MIN;
-
-    if (heapSize == 1)
-    {
-        --heapSize;
-        return heapArr[0];
-    }
-
-    int root = heapArr[0];
-    heapArr[0] = heapArr[heapSize - 1];
-    --heapSize;
-    MaxHeapify(0, heapSize);
-
-    return root;
-}
-
-void MaxHeap::print()
-{
-    std::cout << "{ ";
-
-    for (int i = 0; i < heapSize; ++i)
-        std::cout << heapArr[i] << " ";
-
-    std::cout << "}" << std::endl;
-}
+};
 
 int main()
 {
-    // ************** Max Heap Example
-    MaxHeap HeapA(10);
 
-    HeapA.insert(30);
-    HeapA.insert(20);
-    HeapA.insert(15);
-    HeapA.insert(5);
-    HeapA.insert(10);
-    HeapA.insert(12);
-    HeapA.insert(6);
+    // Create Heap using Insert | Time: O(nlogN)
+    int arr[] = {10, 30, 20, 5, 45, 35, 25};
+    int arrSize = sizeof(arr) / sizeof(arr[0]);
 
-    /*
-        Should look like this:
+    MaxHeap *heap = new MaxHeap(arrSize);
 
-        Tree:
-
-                30
-              /    \
-            20      15
-           /  \    /  \
-          5   10  12   6
-
-        Array:
-
-        [ 30 20 15 5 10 12 6 ]
-    */
-
-    std::cout << "A Max-Heap: ";
-    HeapA.print(); // [ 30 20 15 5 10 12 6 ]
-
-    HeapA.extractMax();
-
-    std::cout << "A Max-Heap (after extractMax function): ";
-    HeapA.print(); // [ 20 10 15 5 6 12 ]
-
-    // ************** Creating Heap from given array using Insert() function
-    std::cout << "\n";
-    int A[] = {10, 20, 25, 5, 40, 35};
-    int aSize = sizeof(A) / sizeof(A[0]);
-
-    MaxHeap HeapB(6);
-
-    for (int i = 0; i < aSize; ++i) // Time Complexity: O(n log(n))
-        HeapB.insert(A[i]);
-
-    std::cout << "B Max-Heap: ";
-    HeapB.print();
-
-    // ************** Creating Heap using Max-Heapify method and implementing Heap Sort
-    std::cout << "\n";
-    int C[] = {10, 20, 25, 5, 40, 35, 15};
-    int cSize = sizeof(C) / sizeof(C[0]);
-    MaxHeap HeapC(cSize, C, cSize);
-
-    std::cout << "C Array (not Max-Heap yet): ";
-    HeapC.print();
-
-    for (int i = cSize / 2 - 1; i >= 0; --i) // Time: O(n)
+    for (int i = 0; i < arrSize; ++i) // Time: O(nlogN)
     {
-        HeapC.MaxHeapify(i, cSize);
+        heap->Insert(arr[i]);
     }
 
-    std::cout << "C Max-Heap (after Max-Heapify): ";
-    HeapC.print();
-
-    // Implementing Heap Sort on created Max Heap
-    auto cArr = HeapC.getHeapArr();
-
-    // Here we starting from the last non-leaf node and keep extracting elements from heap and calling max heapify on reduced size Heap
-    for (int i = cSize - 1; i > 0; --i) // Time Complexity: O(n logn)
-    {
-        std::swap(cArr[0], cArr[i]);
-        HeapC.MaxHeapify(0, i);
-    }
-
-    std::cout << "C Array (after Heap Sort): ";
-    HeapC.print();
+    heap->printHeap(); // [ 45 30 35 5 10 20 25 ]
 
     return 0;
 }
