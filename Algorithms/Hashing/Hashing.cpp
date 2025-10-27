63fd6334ef730599c4b7ab2c3403961b482d007c
Mert Eldemir <mert1greatest@gmail.com>
2025-10-27 14:41:43 +0300

    hashing

---
+++ Algorithms/Hashing/Hashing.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+// Hashing
+// Author: Mert Eldemir
+
+int main()
+{
+    // ********************* Simple Hashing (One-to-One Mapping) *********************
+    int arr[] = {8, 3, 6, 10, 15, 18, 4};
+    int arrSize = sizeof(arr) / sizeof(arr[0]);
+
+    // find max value from the arr
+    int max = INT_MIN;
+    for (int i = 0; i < arrSize; ++i)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    // allocate max + 1 size array
+    int *hashedArr = new int[max + 1]();
+
+    // now each value stays on the same number index, which allows fast access with O(1) time complexity
+    for (int i = 0; i < arrSize; ++i)
+    {
+        hashedArr[arr[i]] = arr[i];
+    }
+
+    std::cout << "[ ";
+    for (int i = 0; i < max + 1; ++i)
+    {
+        std::cout << hashedArr[i] << " ";
+    }
+    std::cout << "]"; // [ 0 0 0 3 4 0 6 0 8 0 10 0 0 0 0 15 0 0 18 ]
+
+    // DRAWBACKS: Allocated array might be much longer if the range of the values is big, which cause bad effect on space complexity
+
+    delete[] hashedArr;
+
+    // ********************* Hashing using Mod operator (Many-to-One Mapping) *********************
+    int arr2[] = {8, 3, 6, 10, 15, 18, 4};
+    int arrSize2 = sizeof(arr2) / sizeof(arr2[0]);
+
+    int hashedArr2[10] = {};
+    for (int i = 0; i < arrSize2; ++i)
+    {
+        hashedArr2[arr2[i] % 10] = arr2[i];
+    }
+
+    // DRAWBACKS: Same remainder may cause to get into the same index, that's called Hash Collision
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -388,7 +388,9 @@ A **Sorting Algorithm** is used to rearrange a given array or list of elements i
 
 Hashing refers to the process of **generating a small sized output** (that can be used as index in a table) from an input of typically large and variable size. Hashing uses **mathematical formulas** known as **hash functions** to do the transformation. This technique determines an **index** or **location** for the storage of an item in a data structure called Hash Table.
 
-- foo
+- Hashing functions
+- Mapping types **one-one**, **one-many** etc.
+- How to solve Hash Collision - **Open Hashing** & **Closed Hashing**
 
   Folder: [Hashing](Algorithms/Hashing)
   </br>
