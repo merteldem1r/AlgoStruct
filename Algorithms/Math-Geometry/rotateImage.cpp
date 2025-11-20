f1e9389d2a4be3a0dc7f4f1cfbddbdabc1454caf
Mert Eldemir <mert1greatest@gmail.com>
2025-11-20 19:58:16 +0300

    rotateImage other solutions

--- Algorithms/Math-Geometry/rotateImage.cpp
+++ Algorithms/Math-Geometry/rotateImage.cpp
@@ -17,6 +17,77 @@ Example 2:
     Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
 */
 
+class SolutionBruteForce // Time: O(n^2) Space: O(n^2)
+{
+public:
+    void rotate(std::vector<std::vector<int>> &matrix)
+    {
+        // Brute Force
+        const int n = matrix.size();
+        std::vector<std::vector<int>> res(n, std::vector<int>(n));
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                res[j][n - i - 1] = matrix[i][j];
+            }
+        }
+
+        matrix = res;
+    }
+};
+
+class SolutionRotationCells // Time: O(n^2) Space: O(1)
+{
+public:
+    void rotate(std::vector<std::vector<int>> &matrix)
+    {
+        int l = 0, r = matrix.size() - 1;
+
+        while (l < r)
+        {
+            for (int i = 0; i < r - l; ++i)
+            {
+                int t = l, b = r;
+
+                int tempTL = matrix[t][l + i];
+
+                // bl -> tl
+                matrix[t][l + i] = matrix[b - i][l];
+
+                // br -> bl
+                matrix[b - i][l] = matrix[b][r - i];
+
+                // tr -> br
+                matrix[b][r - i] = matrix[t + i][r];
+
+                // tempTL -> tr
+                matrix[t + i][r] = tempTL;
+            }
+            ++l, --r;
+        }
+    }
+};
+
+class SolutionReverseAndTranspose // Time: O(n^2) Space: O(1)
+{
+public:
+    void rotate(std::vector<std::vector<int>> &matrix)
+    {
+        const int n = matrix.size();
+        std::reverse(matrix.begin(), matrix.end());
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i + 1; j < n; ++j)
+            {
+                std::swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+};
+
 int main()
 {
     return 0;
