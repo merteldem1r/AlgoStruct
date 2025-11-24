c60b30c064e1d0eeff0a80461abbea44aadf3854
Mert Eldemir <mert1greatest@gmail.com>
2025-11-24 11:52:31 +0300

    LeetCode: 875. Koko Eating Bananas

---
+++ Algorithms/Binary-Search/kokoEatingBananas.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+// LeetCode: 875. Koko Eating Bananas
+// https://leetcode.com/problems/koko-eating-bananas/description/
+
+/*
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+
+Example 1:
+    Input: piles = [3,6,7,11], h = 8
+    Output: 4
+
+Example 2:
+    Input: piles = [30,11,23,4,20], h = 5
+    Output: 30
+
+Example 3:
+    Input: piles = [30,11,23,4,20], h = 6
+    Output: 23
+*/
+
+class Solution // Time: O(m * logn) Space: O(1)
+{
+public:
+    int minEatingSpeed(std::vector<int> &piles, int h)
+    {
+        const int n = piles.size();
+        int max = *std::max_element(piles.begin(), piles.end());
+
+        int l = 1, r = max, res = 0;
+        while (l <= r)
+        {
+            long long time = 0;
+            int k = l + (r - l) / 2;
+
+            for (auto pile : piles)
+            {
+                time += (pile + k - 1) / k;
+            }
+
+            if (time <= h)
+            {
+                res = k;
+                r = k - 1;
+            }
+            else
+            {
+                l = k + 1;
+            }
+        }
+
+        return res;
+    }
+};
+
+int main()
+{
+    return 0;
+}
