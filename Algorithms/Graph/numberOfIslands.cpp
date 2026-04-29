fd88d815816b5f3cdb241054250d51523e132f0d
Mert <mert1greatest@gmail.com>
2026-04-29 15:46:24 +0300

    leetcode 200. number of islands bfs

--- Algorithms/Graph/numberOfIslands.cpp
+++ Algorithms/Graph/numberOfIslands.cpp
@@ -69,6 +69,60 @@ public:
     }
 };
 
+// Breadth First Search (BFS) Solution
+class SolutionBFS // Time: O(m * n) || Space: O(m * n)
+{
+private:
+    const int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+public:
+    void bfsUtil(std::vector<std::vector<char>> &grid, int r, int c)
+    {
+        std::queue<std::pair<int, int>> q;
+
+        q.push({r, c});
+        grid[r][c] = '0';
+
+        while (!q.empty())
+        {
+            auto [fr, fc] = q.front();
+            q.pop();
+
+            for (int i = 0; i < 4; ++i)
+            {
+                const int nr = fr + directions[i][0];
+                const int nc = fc + directions[i][1];
+                if (nr < grid.size() && nc < grid[0].size() && nr >= 0 &&
+                    nc >= 0 && grid[nr][nc] == '1')
+                {
+                    grid[nr][nc] = '0';
+                    q.push({nr, nc});
+                }
+            }
+        }
+    }
+
+    int numIslands(std::vector<std::vector<char>> &grid)
+    {
+        const int rowCount = grid.size(), colCount = grid[0].size();
+        int res = 0;
+
+        for (int i = 0; i < rowCount; ++i)
+        {
+            for (int j = 0; j < colCount; ++j)
+            {
+                if (grid[i][j] == '1')
+                {
+                    ++res;
+                    bfsUtil(grid, i, j);
+                }
+            }
+        }
+
+        return res;
+    }
+};
+
 int main()
 {
     return 0;
