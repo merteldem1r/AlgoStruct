fc86ce603112c9a0998d1d3451ced548a316b5e5
Mert <mert1greatest@gmail.com>
2026-05-21 17:42:46 +0300

    0/1 Knapsack rec solution

--- Algorithms/Dynamic-Programming/0-1-Knapsack.cpp
+++ Algorithms/Dynamic-Programming/0-1-Knapsack.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 // 0/1 Knapsack Problem - Dynamic Programming
+// For reading: https://www.geeksforgeeks.org/dsa/0-1-knapsack-problem-dp-10/
 
 /*
 The 0/1 Knapsack Problem is a optimization problem.
@@ -23,8 +24,39 @@ So each item has only two choices:
     1 → take the item
 */
 
+int knapsackRecursive(const std::vector<int> &values, const std::vector<int> &wt, int idx, int remainCap) // Time: O(2^n) Space: O(n)
+{
+    if (remainCap == 0 || idx >= wt.size())
+    {
+        return 0;
+    }
+
+    // skip index when exceed remain capacity
+    if (wt[idx] > remainCap)
+    {
+        return knapsackRecursive(values, wt, idx + 1, remainCap);
+    }
+
+    // case 1: skip item
+    int skip = knapsackRecursive(values, wt, idx + 1, remainCap);
+
+    // case 2: take item
+    int take = values[idx] + knapsackRecursive(values, wt, idx + 1, remainCap - wt[idx]);
+
+    return std::max(skip, take);
+}
+
 int main()
 {
+    std::vector<int> values = {1, 4, 5, 7};
+    std::vector<int> weights = {1, 3, 4, 5};
+
+    int cap = 7;
+
+    // 1: Naive (recursive)
+    int resRecursive = knapsackRecursive(values, weights, 0, cap);
+
+    std::cout << "max value: " << resRecursive << std::endl;
 
     return 0;
 }
